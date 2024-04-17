236f41a093574ddd474972daeb3ad7b0f0f72761
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-17 06:45:34 +0100

    Return the status of the API

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains the status route
+"""
+from flask import jsonify
+from . import app_views
+
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def status():
+    """
+    Return the status of the API
+    """
+    return jsonify({"status": "OK"})
