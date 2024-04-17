e25c924ea072d65a6c4699672672ce0ba4f98462
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-17 07:20:21 +0100

    Return the number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ This module contains the status route
 """
 from flask import jsonify
 from . import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
@@ -12,3 +13,20 @@ def status():
     Return the status of the API
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stats():
+    """
+    Return the number of objects by type
+    """
+    total_objects = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    return jsonify(total_objects)
