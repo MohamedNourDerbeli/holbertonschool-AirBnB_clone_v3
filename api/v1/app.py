7025945e58f9ba31b9d0efc4ae439d93a4a97329
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-17 07:37:36 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 This is the main file
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
@@ -17,6 +17,14 @@ def close_storage(exception=None):
     Closes the storage on teardown
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle 404 error
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
