74acab08fa7001c4cbab2e366100ea65a53c4bdd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-17 08:46:02 +0100

    Handle all State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 This module contains the states route
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.state import State
 
@@ -37,3 +37,55 @@ def state(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+def delete_state(state_id):
+    """
+    :param state_id:
+    :return:
+    state_id: string - the state id
+    Return:
+    a json representation of the state
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
+def create_state():
+    """
+    :return:
+    Return:
+    a json representation of the state
+    """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def update_state(state_id):
+    """update a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state, key, value)
+    storage.save()
