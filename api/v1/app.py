cb0ebc9ef39d70bd4985ab2955eb23847605dfc7
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-18 21:12:16 +0100

    add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,11 @@ from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
