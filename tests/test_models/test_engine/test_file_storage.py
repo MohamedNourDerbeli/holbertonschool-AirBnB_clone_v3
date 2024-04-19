c3437159277a1bf09c777ae7fdb2296113b657a9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-19 05:10:54 +0100

    all test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -23,6 +23,58 @@ storage_type = os.environ.get("HBNB_TYPE_STORAGE")
 
 
 @unittest.skipIf(storage_type == "db", "skip if environ is db")
+class TestFileStorageDocs(unittest.TestCase):
+    """Class for testing BaseModel docs"""
+
+    @classmethod
+    def setUpClass(cls):
+        print("\n\n.................................")
+        print("..... Testing Documentation .....")
+        print("..... For FileStorage Class .....")
+        print(".................................\n\n")
+
+    def test_doc_file(self):
+        """... documentation for the file"""
+        expected = (
+            "serializes instances to a JSON file \
+& deserializes back to instances"
+        )
+        actual = models.FileStorage.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_class(self):
+        """... documentation for the class"""
+        expected = "serializes instances to a JSON file & \
+deserializes back to instances"
+        actual = FileStorage.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_all(self):
+        """returns the dictionary __objects"""
+        expected = "returns the dictionary __objects"
+        actual = FileStorage.all.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_new(self):
+        """... documentation for new function"""
+        expected = "sets in __objects the obj with key <obj class name>.id"
+        actual = FileStorage.new.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_save(self):
+        """... documentation for save function"""
+        expected = "serializes __objects to the JSON file (path: __file_path)"
+        actual = FileStorage.save.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_reload(self):
+        """... documentation for reload function"""
+        expected = "deserializes the JSON file to __objects"
+        actual = FileStorage.reload.__doc__
+        self.assertEqual(expected, actual)
+
+
+@unittest.skipIf(storage_type == "db", "skip if environ is db")
 class TestBmFsInstances(unittest.TestCase):
     """testing for class instances"""
 
